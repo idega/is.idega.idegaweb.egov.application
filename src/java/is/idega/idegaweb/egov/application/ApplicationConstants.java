cde75586248270efae3ad743f3de9680582ce41e
Thorhallur Helgason <laddi@idega.com>
2007-02-15 09:57:45 +0000

    Declaring new block version.

---
+++ src/java/is/idega/idegaweb/egov/application/ApplicationConstants.java
@@ -0,0 +1,17 @@
+/*
+ * $Id$
+ * Created on Feb 12, 2007
+ *
+ * Copyright (C) 2007 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ */
+package is.idega.idegaweb.egov.application;
+
+
+public class ApplicationConstants {
+
+	public static final String PROPERTY_SHOW_SIBLING_INFORMATION = "egov.application.sibling.info";
+	
+}
