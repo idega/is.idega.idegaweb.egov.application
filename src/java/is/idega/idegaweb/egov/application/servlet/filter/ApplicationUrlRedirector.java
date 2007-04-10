3836732d66ec6b0dabbc5fbcf0a67d65f01b0eec
Vytautas Civilis <civilis@idega.com>
2007-04-10 09:54:06 +0000

    *** empty log message ***

--- src/java/is/idega/idegaweb/egov/application/servlet/filter/ApplicationUrlRedirector.java
+++ src/java/is/idega/idegaweb/egov/application/servlet/filter/ApplicationUrlRedirector.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ApplicationUrlRedirector.java,v 1.9 2007/02/21 12:50:13 tryggvil Exp $ Created on
+ * $Id: ApplicationUrlRedirector.java,v 1.10 2007/04/10 09:54:06 civilis Exp $ Created on
  * Jan 17, 2006
  * 
  * Copyright (C) 2006 Idega Software hf. All Rights Reserved.
@@ -47,7 +47,7 @@ public class ApplicationUrlRedirector extends BaseFilter implements Filter  {
 			ServletException {
 		HttpServletRequest request = (HttpServletRequest) srequest;
 		HttpServletResponse response = (HttpServletResponse) sresponse;
-		initializeDefaultDomain(request);
+//		TODO: should this be removed or replaced? initializeDefaultDomain(request);
 		boolean doRedirect = getIfDoRedirect(request);
 		if (doRedirect) {
 			String newUrl = getNewRedirectURL(request,response);
