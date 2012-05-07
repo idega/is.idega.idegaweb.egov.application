d29ca03188499ebf9933ea3b9f29b324fdc24038
Valdas Žemaitis <valdas@idega.com>
2012-05-07 10:32:40 +0300

    Checking if IWContext is not null

--- src/java/is/idega/idegaweb/egov/application/business/ApplicationBusinessBean.java
+++ src/java/is/idega/idegaweb/egov/application/business/ApplicationBusinessBean.java
@@ -1,9 +1,9 @@
 /*
  * $Id: ApplicationBusinessBean.java,v 1.21 2008/12/12 11:00:57 valdas Exp $
  * Created on Jan 12, 2006
- * 
+ *
  * Copyright (C) 2006 Idega Software hf. All Rights Reserved.
- * 
+ *
  * This software is the proprietary information of Idega hf. Use is subject to
  * license terms.
  */
@@ -37,6 +37,7 @@ import com.idega.core.localisation.business.ICLocaleBusiness;
 import com.idega.data.IDOLookup;
 import com.idega.data.IDOLookupException;
 import com.idega.data.IDORuntimeException;
+import com.idega.idegaweb.IWMainApplication;
 import com.idega.presentation.IWContext;
 import com.idega.user.business.UserBusiness;
 import com.idega.user.business.group.GroupsFilterEngine;
@@ -52,24 +53,28 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 
 	private static final long serialVersionUID = 3838991543604095561L;
 
+	@Override
 	public Application getApplication(String caseCode) throws FinderException {
 		return getApplicationHome().findByCaseCode(getCaseCode(caseCode));
 	}
-	
+
+	@Override
 	public Application getApplication(Object primaryKey) throws FinderException {
 		return getApplicationHome().findByPrimaryKey(primaryKey);
 	}
-	
+
+	@Override
 	public void updateTimesClicked(Application application) {
 		int timesClicked = application.getTimesClicked() + 1;
 		if (timesClicked == 0) {
 			timesClicked++;
 		}
-		
+
 		application.setTimesClicked(timesClicked);
 		application.store();
 	}
-	
+
+	@Override
 	public void checkApplicationCategoryPriorityConstraint() {
 		Collection categories = null;
 		try {
@@ -84,19 +89,19 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 			for(Iterator<ApplicationCategory> it = categories.iterator(); it.hasNext(); ) {
 				ApplicationCategory cat = it.next();
 				Integer priority = cat.getPriority();
-				
+
 				if(priority == null) {
 					unprioritized.add(cat);
 					continue;
 				}
-				
+
 				if(priority.intValue() != currentPriority) {
 					cat.setPriority(currentPriority);
 					cat.store();
 				}
 				currentPriority++;
 			}
-			
+
 			for(Iterator<ApplicationCategory> it = unprioritized.iterator(); it.hasNext(); ) {
 				ApplicationCategory cat = it.next();
 				cat.setPriority(currentPriority);
@@ -105,7 +110,8 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 			}
 		}
 	}
-	
+
+	@Override
 	public void checkApplicationPriorityConstraint(ApplicationCategory cat) {
 		Collection apps = null;
 		try {
@@ -120,19 +126,19 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 			for(Iterator<Application> it = apps.iterator(); it.hasNext(); ) {
 				Application app = it.next();
 				Integer priority = app.getPriority();
-				
+
 				if(priority == null) {
 					unprioritized.add(app);
 					continue;
 				}
-				
+
 				if(priority.intValue() != currentPriority) {
 					app.setPriority(currentPriority);
 					app.store();
 				}
 				currentPriority++;
 			}
-			
+
 			for(Iterator<Application> it = unprioritized.iterator(); it.hasNext(); ) {
 				Application app = it.next();
 				app.setPriority(currentPriority);
@@ -141,7 +147,8 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 			}
 		}
 	}
-	
+
+	@Override
 	public Collection getMostClickedApplications(int numberOfEntries) {
 		try {
 			return getApplicationHome().findMostClicked(numberOfEntries);
@@ -151,13 +158,14 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 			return new ArrayList();
 		}
 	}
-	
+
+	@Override
 	public Collection getUserApplications(User user) {
 		Collection list = new ArrayList();
 		try {
 			Age[] ages = getAgesForUserAndChildren(user);
 			Collection applications = getApplicationHome().findElectronicApplications();
-			
+
 			Iterator iter = applications.iterator();
 			while (iter.hasNext()) {
 				Application application = (Application) iter.next();
@@ -169,10 +177,11 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 		catch (FinderException fe) {
 			fe.printStackTrace();
 		}
-		
+
 		return list;
 	}
 
+	@Override
 	public Age[] getAgesForUserAndChildren(User user) {
 		FamilyLogic famLog = null;
 		try {
@@ -194,7 +203,7 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 		} catch (RemoteException e) {
 			ages = new Age[1];
 		}
-			
+
 		int i = 0;
 		Date date = user.getDateOfBirth();
 		if (date != null) {
@@ -203,7 +212,7 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 		i++;
 		if(coll != null) {
 			iter = coll.iterator();
-			
+
 			while (iter.hasNext()) {
 				User child = (User) iter.next();
 				date = child.getDateOfBirth();
@@ -212,21 +221,22 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 				}
 				i++;
 			}
-		}		
+		}
 		return ages;
 	}
 
+	@Override
 	public boolean displayApplicationForAges(Application application, Age[] ages) {
 		if (ages == null) {
 			return false;
 		}
-		
+
 		int from = application.getAgeFrom();
 		int to = application.getAgeTo();
 		if (from < 0 && to < 0) {
 			return true;
 		}
-		
+
 		for (int i = 0; i < ages.length; i++) {
 			if (ages[i] != null) {
 				int years = ages[i].getYears();
@@ -237,7 +247,8 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 		}
 		return false;
 	}
-	
+
+	@Override
 	public boolean canApplyForApplication(String casecode, User user) {
 		try {
 			Application application = getApplication(casecode);
@@ -254,7 +265,7 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 				IWTimestamp stamp = new IWTimestamp(user.getDateOfBirth());
 				stamp.setDay(1);
 				stamp.setMonth(1);
-				
+
 				Age age = new Age(stamp.getDate());
 				int years = age.getYears();
 				if (from <= years && to >= years) {
@@ -268,6 +279,7 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 		return false;
 	}
 
+	@Override
 	public ApplicationHome getApplicationHome() {
 		try {
 			return (ApplicationHome) IDOLookup.getHome(Application.class);
@@ -277,6 +289,7 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 		}
 	}
 
+	@Override
 	public ApplicationCategoryHome getApplicationCategoryHome() {
 		try {
 			return (ApplicationCategoryHome) IDOLookup.getHome(ApplicationCategory.class);
@@ -286,6 +299,7 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 		}
 	}
 
+	@Override
 	@SuppressWarnings("unchecked")
 	public Collection<Application> getAvailableApplications(IWContext iwc, String caseCode) {
 		Collection<Application> applications = null;
@@ -294,34 +308,33 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 		} catch (FinderException e) {
 			e.printStackTrace();
 		}
-		if (ListUtil.isEmpty(applications)) {
+		if (ListUtil.isEmpty(applications))
 			return null;
-		}
-		if (iwc.isSuperAdmin()) {
+
+		if (iwc != null && iwc.isSuperAdmin())
 			return applications;
-		}
-		
+
 		List<Application> availableApplications = new ArrayList<Application>();
 		User currentUser = null;
 		try {
-			currentUser = iwc.getCurrentUser();
+			currentUser = iwc == null ? null : iwc.getCurrentUser();
 		} catch(NotLoggedOnException e) {
 			e.printStackTrace();
 		}
 		UserBusiness userBusiness = null;
 		try {
-			userBusiness = (UserBusiness) IBOLookup.getServiceInstance(iwc, UserBusiness.class);
+			userBusiness = (UserBusiness) IBOLookup.getServiceInstance(IWMainApplication.getDefaultIWApplicationContext(), UserBusiness.class);
 		} catch (IBOLookupException e) {
 			e.printStackTrace();
 		}
-		
+
 		GroupsFilterEngine groupsFilter = ELUtil.getInstance().getBean(GroupsFilterEngine.SPRING_BEAN_IDENTIFIER);
 		Collection<Group> userGroups = groupsFilter.getUserGroups(iwc, false);
-		
+
 		Collection<Group> groups = null;
 		for (Application app: applications) {
 			groups = app.getGroups();
-			
+
 			if (ListUtil.isEmpty(groups)) {
 				availableApplications.add(app);
 			}
@@ -332,7 +345,7 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 					for (Iterator<Group> it = groups.iterator(); (it.hasNext() && !applicationAdded);) {
 						group = it.next();
 						try {
-							if (userBusiness.isGroupUnderUsersTopGroupNode(iwc, group, currentUser, userGroups) || 
+							if (userBusiness.isGroupUnderUsersTopGroupNode(iwc, group, currentUser, userGroups) ||
 									userBusiness.isMemberOfGroup(Integer.valueOf(group.getId()), currentUser)) {
 								applicationAdded = true;
 								availableApplications.add(app);
@@ -346,10 +359,11 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 				}
 			}
 		}
-		
+
 		return availableApplications;
 	}
-	
+
+	@Override
 	public String getApplicationName(Application application, Locale locale) {
 		String name = null;
 		//	1
@@ -357,12 +371,12 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 		if (locText != null) {
 			name = locText.getBody();
 		}
-		
+
 		//	2
 		if (StringUtil.isEmpty(name)) {
 			name = application.getLocalizedName(locale);
 		}
-		
+
 		//	3
 		if (StringUtil.isEmpty(name)) {
 			name = application.getNameByLocale(locale);
@@ -372,21 +386,22 @@ public class ApplicationBusinessBean extends CaseBusinessBean implements CaseBus
 		if (StringUtil.isEmpty(name)) {
 			name = application.getName();
 		}
-		
+
 		return name;
 	}
 
+	@Override
 	public Collection<Application> getApplicationsByType(String type) {
 		if (StringUtil.isEmpty(type)) {
 			return null;
 		}
-		
+
 		try {
 			return getApplicationHome().findAllByType(type);
 		} catch (FinderException e) {
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
 }
