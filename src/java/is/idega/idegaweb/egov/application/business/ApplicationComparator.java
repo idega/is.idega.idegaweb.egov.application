3a78e9835b3b8c156099220b70d3f4282fa3d0b6
Valdas Žemaitis <valdas@idega.com>
2015-01-15 19:40:42 +0200

    Improved comparator

--- src/java/is/idega/idegaweb/egov/application/business/ApplicationComparator.java
+++ src/java/is/idega/idegaweb/egov/application/business/ApplicationComparator.java
@@ -10,26 +10,24 @@
 package is.idega.idegaweb.egov.application.business;
 
 import is.idega.idegaweb.egov.application.data.Application;
+
 import java.text.Collator;
 import java.util.Comparator;
 import java.util.Locale;
 
+public class ApplicationComparator implements Comparator<Application> {
 
-public class ApplicationComparator implements Comparator {
+	private Locale locale = null;
+	private Collator collator = null;
 
-	private Locale iLocale;
-	
 	public ApplicationComparator(Locale locale) {
-		this.iLocale = locale;
+		this.locale = locale;
+		collator = Collator.getInstance(locale);
 	}
-	
-	public int compare(Object o1, Object o2) {
-		Collator coll = Collator.getInstance(this.iLocale);
-		
-		Application application1 = (Application) o1;
-		Application application2 = (Application) o2;
-		
-//		return coll.compare(application1.getName(), application2.getName());
-		return coll.compare(application1.getNameByLocale(), application2.getNameByLocale());
+
+	@Override
+	public int compare(Application app1, Application app2) {
+		return collator.compare(app1.getNameByLocale(locale), app2.getNameByLocale(locale));
 	}
+
 }
