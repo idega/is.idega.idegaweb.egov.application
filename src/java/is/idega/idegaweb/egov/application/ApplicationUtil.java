3ed5156ae57f590ad8fcc582acc4dfaf12c2bfbe
Valdas Žemaitis <valdas@idega.com>
2016-08-09 11:02:03 +0300

    Moved methods to util class

--- src/java/is/idega/idegaweb/egov/application/ApplicationUtil.java
+++ src/java/is/idega/idegaweb/egov/application/ApplicationUtil.java
@@ -1,26 +1,42 @@
 package is.idega.idegaweb.egov.application;
 
+import java.net.URLEncoder;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.faces.component.UIComponent;
+import javax.servlet.http.HttpServletRequest;
+
 import com.idega.block.text.model.LocalizedTextModel;
+import com.idega.builder.business.BuilderLogic;
+import com.idega.core.builder.data.ICPage;
 import com.idega.core.localisation.business.ICLocaleBusiness;
 import com.idega.data.IDOLookup;
+import com.idega.data.SimpleQuerier;
+import com.idega.idegaweb.IWMainApplication;
 import com.idega.idegaweb.IWMainApplicationSettings;
 import com.idega.presentation.IWContext;
 import com.idega.repository.data.Singleton;
+import com.idega.servlet.filter.IWAuthenticator;
+import com.idega.util.ArrayUtil;
+import com.idega.util.CoreConstants;
 import com.idega.util.CoreUtil;
 import com.idega.util.IWTimestamp;
 import com.idega.util.ListUtil;
 import com.idega.util.StringUtil;
+import com.idega.util.URIUtil;
 import com.idega.util.expression.ELUtil;
 
+import is.idega.idegaweb.egov.application.business.ApplicationType;
+import is.idega.idegaweb.egov.application.business.ApplicationTypesManager;
 import is.idega.idegaweb.egov.application.data.Application;
 import is.idega.idegaweb.egov.application.data.ApplicationBMPBean;
 import is.idega.idegaweb.egov.application.data.ApplicationCategory;
@@ -29,10 +45,192 @@ import is.idega.idegaweb.egov.application.data.ApplicationHome;
 import is.idega.idegaweb.egov.application.data.dao.ApplicationDAO;
 import is.idega.idegaweb.egov.application.model.ApplicationCategoryModel;
 import is.idega.idegaweb.egov.application.model.ApplicationModel;
+import is.idega.idegaweb.egov.application.presentation.ApplicationBlock;
+import is.idega.idegaweb.egov.application.presentation.ApplicationFormInIframe;
+import is.idega.idegaweb.egov.application.presentation.DisabledApplicationView;
+import is.idega.idegaweb.egov.application.servlet.filter.ApplicationUrlRedirector;
 
 public class ApplicationUtil implements Singleton {
 
 	private static final Logger LOGGER = Logger.getLogger(ApplicationUtil.class.getName());
+
+	private static String getUriForModule(IWMainApplication iwma, Class<? extends UIComponent> uiClass, String appPropName) {
+		String uri = null;
+		List<ICPage> pagesWithModule = null;
+		try {
+			pagesWithModule = BuilderLogic.getInstance().findPagesForModule(uiClass);
+		} catch (Exception e) {
+			LOGGER.log(Level.WARNING, "Error looking up for pages with module " + uiClass.getName(), e);
+		}
+		if (ListUtil.isEmpty(pagesWithModule)) {
+			uri = iwma.getSettings().getProperty(appPropName, CoreConstants.PAGES_URI_PREFIX);
+			LOGGER.warning("Did not find page for module: " + uiClass.getName() + ", using app property value (" + uri + "). Will clear all caches");
+			CoreUtil.clearAllCaches();
+			return uri;
+		}
+
+		ICPage page = null;
+		for (Iterator<ICPage> pagesIter = pagesWithModule.iterator(); (pagesIter.hasNext() && page == null); pagesIter.hasNext()) {
+			page = pagesIter.next();
+			if (page.getDeleted()) {
+				page = null;
+			}
+		}
+		if (page == null) {
+			uri = iwma.getSettings().getProperty(appPropName, CoreConstants.PAGES_URI_PREFIX);
+			LOGGER.warning("Did not find not deleted page for module: " + uiClass.getName() + ", using app property value (" + uri + ")");
+			return uri;
+		} else {
+			uri = CoreConstants.PAGES_URI_PREFIX + page.getDefaultPageURI();
+		}
+
+		return uri;
+	}
+
+	public static boolean isAvailabe(IWContext iwc, ApplicationModel application) {
+		if (application == null) {
+			LOGGER.warning("Application is not provided!");
+			return false;
+		}
+
+		if ((!application.isEnabled() || !application.getVisible()) && !iwc.isSuperAdmin()) {
+			return false;
+		}
+		return true;
+	}
+
+	public static String getRedirectUrl(
+			IWMainApplication iwma,
+			IWContext iwc,
+			HttpServletRequest request,
+			ApplicationTypesManager appTypesManager,
+			ApplicationModel application,
+			String pk,
+			boolean isLoggedOn
+	) {
+		if (application == null) {
+			LOGGER.warning("Application is not provided!");
+			return CoreConstants.PAGES_URI_PREFIX;
+		}
+
+		try {
+			String url = null;
+			if (!isAvailabe(iwc, application)) {
+				String uri = getUriForModule(iwma, DisabledApplicationView.class, "disabled_app_page");
+				URIUtil util = new URIUtil(uri);
+				util.setParameter(ApplicationConstants.PARAM_APP_ID, pk);
+				return util.getUri();
+			}
+
+			String appType = application.getAppType();
+			if (StringUtil.isEmpty(appType)) {
+				LOGGER.info("App type is unknown, will execute raw query to double-check it");
+				String query = "select app_type from EGOV_APPLICATION where EGOV_APPLICATION_ID = " + pk;
+				try {
+					String[] results = SimpleQuerier.executeStringQuery(query);
+					if (!ArrayUtil.isEmpty(results)) {
+						appType = results[0];
+					}
+				} catch (Exception e) {
+					LOGGER.log(Level.WARNING, "Error gettin app type for application with ID: " + pk);
+				}
+			}
+
+			if (application.getShowInIframe()) {
+				String uri = getUriForModule(iwma, ApplicationFormInIframe.class, "form_in_iframe_page");
+				URIUtil util = new URIUtil(uri);
+				util.setParameter(ApplicationConstants.PARAM_APP_ID, pk);
+				return util.getUri();
+			} else if (StringUtil.isEmpty(appType)) {
+				LOGGER.info("App type was not resolved");
+				url = application.getUrlByLocale(iwc.getCurrentLocale());
+			} else {
+				ApplicationType at = appTypesManager.getApplicationType(appType);
+				url = at.getUrl(iwc, application);
+			}
+
+			if (StringUtil.isEmpty(url)) {
+				LOGGER.warning("URL is null for " + application.getPrimaryKey() + ", app type: " + appType) ;
+			}
+
+			String encoding = System.getProperty(ApplicationUrlRedirector.PROP_FILE_ENCODING);
+
+			if (application.getElectronic() && application.getRequiresLogin() && !isLoggedOn) {
+				IWMainApplicationSettings settings = iwma.getSettings();
+
+				String loginPage = settings.getProperty(ApplicationUrlRedirector.PROP_LOGIN_PAGE_URI, CoreConstants.PAGES_URI_PREFIX + CoreConstants.SLASH);
+				if (CoreConstants.EMPTY.equals(loginPage)) {
+					//backup
+					loginPage = settings.getProperty(CoreConstants.PAGE_ERROR_403_HANDLER_PORPERTY, CoreConstants.PAGES_URI_PREFIX + CoreConstants.SLASH);
+				}
+
+				if (application.getLoginPageURL() != null) {
+					loginPage = application.getLoginPageURL();
+				}
+
+				StringBuilder uri = new StringBuilder(loginPage);
+				if (loginPage.indexOf(CoreConstants.QMARK) == -1) {
+					uri.append(CoreConstants.QMARK);
+				} else {
+					uri.append(CoreConstants.AMP);
+				}
+
+				StringBuilder appUrlBuilder = new StringBuilder(url);
+				Enumeration<String> enumeration = request.getParameterNames();
+				while (enumeration.hasMoreElements()) {
+					String parameter = enumeration.nextElement();
+
+					if (!parameter.equals(ApplicationBlock.PARAMETER_APPLICATION_PK)) {
+						String[] values = request.getParameterValues(parameter);
+						if (values != null) {
+							for (int i = 0; i < values.length; i++) {
+								String value = values[i];
+								if (url.indexOf(CoreConstants.QMARK) == -1) {
+									appUrlBuilder.append(CoreConstants.QMARK);
+								}
+								else {
+									appUrlBuilder.append(CoreConstants.AMP);
+								}
+								appUrlBuilder.append(parameter).append(CoreConstants.EQ).append(value);
+							}
+						}
+					}
+				}
+
+				String applUrlEncoded = URLEncoder.encode(appUrlBuilder.toString(), encoding);
+				uri.append(IWAuthenticator.PARAMETER_REDIRECT_URI_ONLOGON).append(CoreConstants.EQ).append(applUrlEncoded);
+
+				return uri.toString();
+			} else if (isLoggedOn) {
+				StringBuilder appUrlBuilder = new StringBuilder(url);
+				Enumeration<String> enumeration = request.getParameterNames();
+				while (enumeration.hasMoreElements()) {
+					String parameter = enumeration.nextElement();
+
+					if (!parameter.equals(ApplicationBlock.PARAMETER_APPLICATION_PK)) {
+						String[] values = request.getParameterValues(parameter);
+						if (values != null) {
+							for (String paramVal : values) {
+								if (appUrlBuilder.toString().indexOf(CoreConstants.QMARK) == -1) {
+									appUrlBuilder.append(CoreConstants.QMARK);
+								}
+								else {
+									appUrlBuilder.append(CoreConstants.AMP);
+								}
+								appUrlBuilder.append(parameter).append(CoreConstants.EQ).append(paramVal);
+							}
+						}
+					}
+				}
+
+				return url = appUrlBuilder.toString();
+			}
+		} catch (Exception e) {
+			LOGGER.log(Level.WARNING, "Error getting redirect URL for application " + application, e);
+		}
+
+		return null;
+	}
 
 	public static final boolean isEnabled(ApplicationModel app) {
 		if (app == null) {
--- src/java/is/idega/idegaweb/egov/application/servlet/filter/ApplicationUrlRedirector.java
+++ src/java/is/idega/idegaweb/egov/application/servlet/filter/ApplicationUrlRedirector.java
@@ -10,15 +10,10 @@
 package is.idega.idegaweb.egov.application.servlet.filter;
 
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.faces.component.UIComponent;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -30,48 +25,35 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.idega.builder.business.BuilderLogic;
 import com.idega.business.IBOLookup;
 import com.idega.business.IBOLookupException;
 import com.idega.business.IBORuntimeException;
 import com.idega.core.accesscontrol.business.LoginBusinessBean;
-import com.idega.core.builder.data.ICPage;
-import com.idega.data.SimpleQuerier;
 import com.idega.idegaweb.IWApplicationContext;
 import com.idega.idegaweb.IWMainApplication;
-import com.idega.idegaweb.IWMainApplicationSettings;
 import com.idega.presentation.IWContext;
 import com.idega.servlet.filter.BaseFilter;
-import com.idega.servlet.filter.IWAuthenticator;
-import com.idega.util.ArrayUtil;
 import com.idega.util.CoreConstants;
-import com.idega.util.CoreUtil;
-import com.idega.util.ListUtil;
 import com.idega.util.StringHandler;
-import com.idega.util.StringUtil;
-import com.idega.util.URIUtil;
 import com.idega.util.expression.ELUtil;
 
 import is.idega.idegaweb.egov.application.ApplicationConstants;
 import is.idega.idegaweb.egov.application.ApplicationUtil;
 import is.idega.idegaweb.egov.application.business.ApplicationBusiness;
-import is.idega.idegaweb.egov.application.business.ApplicationType;
 import is.idega.idegaweb.egov.application.business.ApplicationTypesManager;
 import is.idega.idegaweb.egov.application.data.dao.ApplicationDAO;
 import is.idega.idegaweb.egov.application.model.ApplicationModel;
 //import is.idega.idegaweb.egov.application.data.Application;
 import is.idega.idegaweb.egov.application.presentation.ApplicationBlock;
-import is.idega.idegaweb.egov.application.presentation.ApplicationFormInIframe;
-import is.idega.idegaweb.egov.application.presentation.DisabledApplicationView;
 
 public class ApplicationUrlRedirector extends BaseFilter implements Filter {
 
 	private static final Logger LOGGER = Logger.getLogger(ApplicationUrlRedirector.class.getName());
 
-	private static final String PROP_LOGIN_PAGE_URI = "LOGIN_PAGE_URI";
+	public static final String	PROP_LOGIN_PAGE_URI = "LOGIN_PAGE_URI",
+								PROP_FILE_ENCODING = "file.encoding";
 
 	private static final String PROP_UPDATE_TIMES_CLICKED = "egov.application.updateclicks";
-	private static final String PROP_FILE_ENCODING = "file.encoding";
 
 	private static final String PROP_VALUE_UPDATE_TIMES_CLICKED_DISABLED = "disabled";
 
@@ -102,40 +84,7 @@ public class ApplicationUrlRedirector extends BaseFilter implements Filter {
 		return map.containsKey(ApplicationBlock.PARAMETER_APPLICATION_PK);
 	}
 
-	private String getUriForModule(IWMainApplication iwma, Class<? extends UIComponent> uiClass, String appPropName) {
-		String uri = null;
-		List<ICPage> pagesWithModule = null;
-		try {
-			pagesWithModule = BuilderLogic.getInstance().findPagesForModule(uiClass);
-		} catch (Exception e) {
-			LOGGER.log(Level.WARNING, "Error looking up for pages with module " + uiClass.getName(), e);
-		}
-		if (ListUtil.isEmpty(pagesWithModule)) {
-			uri = iwma.getSettings().getProperty(appPropName, CoreConstants.PAGES_URI_PREFIX);
-			LOGGER.warning("Did not find page for module: " + uiClass.getName() + ", using app property value (" + uri + "). Will clear all caches");
-			CoreUtil.clearAllCaches();
-			return uri;
-		}
-
-		ICPage page = null;
-		for (Iterator<ICPage> pagesIter = pagesWithModule.iterator(); (pagesIter.hasNext() && page == null); pagesIter.hasNext()) {
-			page = pagesIter.next();
-			if (page.getDeleted()) {
-				page = null;
-			}
-		}
-		if (page == null) {
-			uri = iwma.getSettings().getProperty(appPropName, CoreConstants.PAGES_URI_PREFIX);
-			LOGGER.warning("Did not find not deleted page for module: " + uiClass.getName() + ", using app property value (" + uri + ")");
-			return uri;
-		} else {
-			uri = CoreConstants.PAGES_URI_PREFIX + page.getDefaultPageURI();
-		}
-
-		return uri;
-	}
-
-	public String getNewRedirectURL(HttpServletRequest request, HttpServletResponse response) {
+	private String getNewRedirectURL(HttpServletRequest request, HttpServletResponse response) {
 		ApplicationModel application = null;
 		try {
 			LoginBusinessBean loginBusiness = getLoginBusiness(request);
@@ -157,126 +106,7 @@ public class ApplicationUrlRedirector extends BaseFilter implements Filter {
 
 			updateTimesClicked(iwma, application);
 
-			String url = null;
-			if ((!application.isEnabled() || !application.getVisible()) && !iwc.isSuperAdmin()) {
-				String uri = getUriForModule(iwma, DisabledApplicationView.class, "disabled_app_page");
-				URIUtil util = new URIUtil(uri);
-				util.setParameter(ApplicationConstants.PARAM_APP_ID, pk);
-				return util.getUri();
-			}
-
-			String appType = application.getAppType();
-			if (StringUtil.isEmpty(appType)) {
-				LOGGER.info("App type is unknown, will execute raw query to double-check it");
-				String query = "select app_type from EGOV_APPLICATION where EGOV_APPLICATION_ID = " + pk;
-				try {
-					String[] results = SimpleQuerier.executeStringQuery(query);
-					if (!ArrayUtil.isEmpty(results)) {
-						appType = results[0];
-					}
-				} catch (Exception e) {
-					LOGGER.log(Level.WARNING, "Error gettin app type for application with ID: " + pk);
-				}
-			}
-
-			if (application.getShowInIframe()) {
-				String uri = getUriForModule(iwma, ApplicationFormInIframe.class, "form_in_iframe_page");
-				URIUtil util = new URIUtil(uri);
-				util.setParameter(ApplicationConstants.PARAM_APP_ID, pk);
-				return util.getUri();
-			} else if (StringUtil.isEmpty(appType)) {
-				LOGGER.info("App type was not resolved");
-				url = application.getUrlByLocale(iwc.getCurrentLocale());
-			} else {
-				ApplicationType at = getApplicationTypesManager().getApplicationType(appType);
-				url = at.getUrl(iwc, application);
-			}
-
-			if (StringUtil.isEmpty(url)) {
-				LOGGER.warning("URL is null for " + application.getPrimaryKey() + ", app type: " + appType) ;
-			}
-
-			String encoding = System.getProperty(PROP_FILE_ENCODING);
-
-			if (application.getElectronic() && application.getRequiresLogin() && !isLoggedOn) {
-				IWMainApplicationSettings settings = iwma.getSettings();
-
-				String loginPage = settings.getProperty(PROP_LOGIN_PAGE_URI, CoreConstants.PAGES_URI_PREFIX + CoreConstants.SLASH);
-				if (CoreConstants.EMPTY.equals(loginPage)) {
-					//backup
-					loginPage = settings.getProperty(CoreConstants.PAGE_ERROR_403_HANDLER_PORPERTY, CoreConstants.PAGES_URI_PREFIX + CoreConstants.SLASH);
-				}
-
-				if (application.getLoginPageURL() != null) {
-					loginPage = application.getLoginPageURL();
-				}
-
-				StringBuilder uri = new StringBuilder(loginPage);
-				if (loginPage.indexOf(CoreConstants.QMARK) == -1) {
-					uri.append(CoreConstants.QMARK);
-				} else {
-					uri.append(CoreConstants.AMP);
-				}
-
-				StringBuilder appUrlBuilder = new StringBuilder(url);
-
-				Enumeration<String> enumeration = request.getParameterNames();
-
-				while (enumeration.hasMoreElements()) {
-					String parameter = enumeration.nextElement();
-
-					if (!parameter.equals(ApplicationBlock.PARAMETER_APPLICATION_PK)) {
-						String[] values = request.getParameterValues(parameter);
-						if (values != null) {
-							for (int i = 0; i < values.length; i++) {
-								String value = values[i];
-								if (/*applUrl*/url.indexOf(CoreConstants.QMARK) == -1) {
-									appUrlBuilder.append(CoreConstants.QMARK);
-								}
-								else {
-									appUrlBuilder.append(CoreConstants.AMP);
-								}
-								appUrlBuilder.append(parameter).append(CoreConstants.EQ).append(value);
-							}
-						}
-					}
-				}
-
-				String applUrlEncoded = URLEncoder.encode(appUrlBuilder.toString(), encoding);
-				uri.append(IWAuthenticator.PARAMETER_REDIRECT_URI_ONLOGON).append(CoreConstants.EQ).append(applUrlEncoded);
-
-				return uri.toString();
-			} else if (isLoggedOn) {
-				StringBuilder appUrlBuilder = new StringBuilder(url);
-
-				Enumeration<String> enumeration = request.getParameterNames();
-
-				while (enumeration.hasMoreElements()) {
-					String parameter = enumeration.nextElement();
-
-					if (!parameter.equals(ApplicationBlock.PARAMETER_APPLICATION_PK)) {
-
-						String[] values = request.getParameterValues(parameter);
-
-						if (values != null) {
-
-							for (String paramVal : values) {
-
-								if (appUrlBuilder.toString().indexOf(CoreConstants.QMARK) == -1) {
-									appUrlBuilder.append(CoreConstants.QMARK);
-								}
-								else {
-									appUrlBuilder.append(CoreConstants.AMP);
-								}
-								appUrlBuilder.append(parameter).append(CoreConstants.EQ).append(paramVal);
-							}
-						}
-					}
-				}
-
-				return url = appUrlBuilder.toString();
-			}
-
+			String url = ApplicationUtil.getRedirectUrl(iwma, iwc, request, getApplicationTypesManager(), application, pk, isLoggedOn);
 			return url;
 		} catch (Exception e) {
 			String message = "Error constructing redirect URL to application " + application;
