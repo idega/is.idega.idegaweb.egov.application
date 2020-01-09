d4a4f753b9d2a1bdbdea1723fedb6ed31b6832b8
Valdas Žemaitis <valdas@idega.com>
2020-01-09 16:16:44 +0200

    Not showing default text for selected disabled applications

---
+++ resources/javascript/DisabledApplicationHelper.js
@@ -0,0 +1,8 @@
+jQuery.noConflict();
+
+jQuery(document).ready(function(){
+	var url = window.location.href;
+	if (url != null && (url.indexOf('?appId=1783') != -1 || url.indexOf('?appId=1781') != -1 || url.indexOf('?appId=1881') != -1)) {
+		jQuery('span.disabledAppTextContainer').css('display', 'none');
+	}
+});
