8e5f8816b989ec4df35bf9599e216a6bda8747f1
Valdas Žemaitis <valdas@idega.com>
2022-03-02 17:21:44 +0200

    Hiding text for one more disabled app.: 1341

--- resources/javascript/DisabledApplicationHelper.js
+++ resources/javascript/DisabledApplicationHelper.js
@@ -2,7 +2,15 @@ jQuery.noConflict();
 
 jQuery(document).ready(function(){
 	var url = window.location.href;
-	if (url != null && (url.indexOf('?appId=1783') != -1 || url.indexOf('?appId=1781') != -1 || url.indexOf('?appId=1881') != -1)) {
+	if (
+		url != null &&
+		(
+			url.indexOf('?appId=1783') != -1 ||
+			url.indexOf('?appId=1781') != -1 ||
+			url.indexOf('?appId=1881') != -1 ||
+			url.indexOf('?appId=1341') != -1
+		)
+	) {
 		jQuery('span.disabledAppTextContainer').css('display', 'none');
 	}
 });
